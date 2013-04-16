d1dfab7687a838de429c8f1ab3da66e0b49008e7
evandinelli <evandinelli12@gmail.com>
2013-04-16 10:44:45 -0500

    First changes:
    Just getting back into VHDL so working on some basic modules for prelab
    1

---
+++ code/working (start here)/robot_arm_control/clk_div.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Create Date:    19:24:55 11/04/2012 
+-- Design Name: HW8 clk divider ( signal vs variable)
+-- Module Name:    clk_div - Behavioral 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_div is
+    Port ( --reset : in  STD_LOGIC;
+           clk_50M : in  STD_LOGIC;
+           clk_1hz : out  STD_LOGIC);
+end clk_div;
+
+architecture Behavioral of clk_div is
+
+signal clk_int : std_logic := '0';
+signal count_1 : integer range 0 to 249 := 0; --- not 250
+
+begin
+----- Method 1: use internal signal count_1
+	process(clk_50M,count_1)
+	begin
+--		if (reset = '1') then
+--				clk_int <= '0';
+--				count_1 <= 0;
+		if ( clk_50M'event and clk_50M = '1') then --rising or falling
+				count_1 <=  count_1 + 1;			
+				if ( count_1 = 249  ) then ----1 Hz  ( not 250)
+					count_1 <= 0;
+					clk_int <= not clk_int;
+				end if;	  
+		end if;
+	end process;
+
+
+----Method 2: use internal variable count_var
+----the variable is local to the process
+--process(reset,clk_50M,count_1)
+--variable count_var : integer range 0 to 250 :=0;
+--begin
+-- if (reset = '1') then
+--			clk_int <= '0';
+--			count_var := 0;---- signal vs variable
+--  elsif ( clk_50M'event and clk_50M = '1') then --rising or falling
+---			count_var :=  count_var + 1;			
+--			if ( count_var = 250 ) then ----1 Hz (exact 250)
+--				count_var := 0;
+--				clk_int <= not clk_int;
+--			end if;	  
+--  end if;
+--end process;
+
+---------------------------------
+clk_1hz <= clk_int;
+
+end Behavioral;
---
+++ code/working (start here)/robot_arm_control/pwm.vhd
@@ -0,0 +1,76 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:47:37 04/15/2013 
+-- Design Name: 
+-- Module Name:    pwm - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pwm is
+
+port(
+		clk         : in    std_logic;
+		reset         : in    std_logic;
+		pwm_on_in   : in    std_logic_vector(31 downto 0);
+                pwm_out     : out   std_logic              
+    );
+
+end pwm;
+
+architecture Behavioral of pwm is
+
+signal num_clks     : unsigned(31 downto 0) := x"00000000"; --1073741824
+signal pwm_width    : unsigned(31 downto 0) := x"0000C350"; --Default to 1ms=50000
+signal pulse_period : unsigned(31 downto 0) := x"000F423F"; --Wait > 20ms 1000000
+ 
+ 
+begin
+pulse_period <= x"000F423F";        --20ms period
+pwm_width <= unsigned(pwm_on_in);   
+ 
+process(clk, reset)
+	begin
+		if(reset = '0') then
+				 num_clks <= (others => '0');
+				 pwm_out <= '0';
+			elsif(rising_edge(clk)) then
+				 if (num_clks < pwm_width) then
+					  pwm_out <= '1';
+					  else 
+					  pwm_out <= '0';
+				 end if;
+			
+				 if(num_clks >= pulse_period) then
+				num_clks <= (others => '0');
+			else
+				num_clks <= num_clks + 1;
+			end if;
+		end if;
+end process;
+
+
+end Behavioral;
+
