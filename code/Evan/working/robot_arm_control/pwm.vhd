a6b7326ab42865a973c1cf192800728629d68cd1
Evan Dinelli <evandinelli12@gmail.com>
2013-04-21 21:41:40 -0500

    Worked on PWM
    Still need to do tb

--- code/Evan/working/robot_arm_control/pwm.vhd
+++ code/Evan/working/robot_arm_control/pwm.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -34,7 +35,7 @@ entity pwm is
 port(
 		clk         : in    std_logic;
 		reset       : in    std_logic;
-		pwm_on_in   : in    std_logic_vector(31 downto 0);
+		--pwm_on_in   : in    std_logic_vector(7 downto 0);
       pwm_out     : out   std_logic              
     );
 
@@ -42,31 +43,30 @@ end pwm;
 
 architecture Behavioral of pwm is
 
-signal num_clks     : unsigned(7 downto 0) := x"0000"; --1073741824
-signal pwm_width    : unsigned(7 downto 0) := x"C350"; --Default to 1ms=50000
-signal pulse_period : unsigned(7 downto 0) := x"000F423F"; --Wait > 20ms 1000000
- 
- 
+signal num_clks     : integer range 0 to 999999 :=0;
+signal pwm_width    : integer range 149 to 194  :=0;
+signal pulse_period : integer range 0 to 999999 :=0;
+
 begin
-pulse_period <= x"000F423F";        --20ms period
-pwm_width <= unsigned(pwm_on_in);   
+pulse_period <= 999999;        --20ms period
+pwm_width <= 149; --CONV_INTEGER(pwm_on_in);   
  
 process(clk, reset)
 	begin
 		if(reset = '0') then
-				 num_clks <= (others => '0');
-				 pwm_out <= '0';
-			elsif(rising_edge(clk)) then
-				 if (num_clks < pwm_width) then
-					  pwm_out <= '1';
-					  else 
-					  pwm_out <= '0';
-				 end if;
+			num_clks <= (others => '0');
+			pwm_out <= '0';
+		elsif(rising_edge(clk)) then
+			if (num_clks < pwm_width) then
+			  pwm_out <= '1';
+			  else 
+			  pwm_out <= '0';
+			end if;
 			
-				 if(num_clks >= pulse_period) then
+			if(num_clks >= pulse_period) then
 				num_clks <= (others => '0');
-			else
-				num_clks <= num_clks + 1;
+		else
+			num_clks <= num_clks + 1;
 			end if;
 		end if;
 end process;
